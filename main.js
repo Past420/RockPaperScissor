32604be9232e3fdc87e52487502f5bb6861f3d34
past <ch.satyasavith23264@gmail.com>
2023-02-25 20:55:05 +0530

    All changes

---
+++ main.js
@@ -0,0 +1,48 @@
+var playerchoice;
+var computerchoice;
+var arr=["rock","paper","scissor"];
+var playerscore = 0;
+var computerscore = 0
+function getchoice()
+{
+    var index = Math.floor(Math.random()*arr.length);
+    return arr[index];
+}
+
+function winner(p,c)
+{
+    if(p===c)
+    {
+        return 0;
+    }
+    else if((p==="paper" && c==="rock")||(p==="scissor" && c==="paper")||(p==="rock" && c==="scissor"))
+    {
+        playerscore++;
+        document.getElementById("p").innerHTML = playerscore;
+    }
+    else
+    {
+        computerscore++;
+        document.getElementById("c").innerHTML = computerscore;
+    }
+}
+function check(event)
+{
+    playerchoice=event;
+    computerchoice = getchoice();
+    winner(playerchoice,computerchoice)
+    if(playerscore===5)
+    {
+        playerscore = 0;
+        computerscore = 0;
+        document.getElementById("c").innerHTML = computerscore;
+        document.getElementById("p").innerHTML = playerscore;
+    }
+    else if(computerscore==5)
+    {
+        playerscore = 0;
+        computerscore = 0;
+        document.getElementById("c").innerHTML = computerscore;
+        document.getElementById("p").innerHTML = playerscore;
+    }
+}
